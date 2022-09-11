db454b62bf5bfba62d6955f5f969ffc25ac5d12c
Acer <Acer@DESKTOP-5753AS8>
2022-09-11 12:16:32 -0300

    Adicionar arquivos de projeto.

---
+++ geometricas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("calcula a area de um  retangulo");
+            Console.Write("informe a altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("informe a base/largura: " );
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            retangulo.ExibeDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ geometricas/Retangulo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        public Retangulo()
+        {
+            this.Altura = 0;
+            this.Base = 0;
+
+        }
+        /*base do retangulo*/
+        private Double baseret;
+
+        public Double Base
+        {
+            get { return baseret; }
+            set { 
+                if (value >= 0 ) baseret = value;
+                else baseret = 0;
+            }
+        }
+        /*altura  do  retangulo*/
+
+        private Double altret;
+
+        public Double Altura
+        {
+            get { return altret; }
+            set {
+                if ( value >= 0 ) altret = value; 
+                else altret = 0;
+
+
+            }
+
+        }
+
+        public Double Area
+        {
+            get { return altret * baseret; }
+
+        }   
+        
+        public void ExibeDados()
+        {
+            Console.WriteLine("Base:   " + this.Base);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Altura: " + this.Area);
+
+
+           
+        }
+
+
+    }
+
+}
+ 
+
